018e6805c471cfb695c84ade8f3db914f529e6c7
= <=>
2024-04-24 22:00:24 -0300

    Implementando o Quiz

--- js/app.js
+++ js/app.js
@@ -1,36 +1,94 @@
-var audio = document.getElementById("myAudio");
-var playing = false;
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+const botao__reiniciar = document.querySelector(".botao__reiniciar");
+const img_happy = document.querySelector(".imagem__final__feliz");
+const img_sad = document.querySelector(".imagem__final__triste");
 
-document.getElementById("playPauseButton").addEventListener("click", function() {
-  if (!playing) {
-    audio.play();
-    playing = true;
+import questions from "./questoes.js";
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+btnRestart.onclick = () => {
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
+
+function nextQuestion(e) {
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  }
+
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
   } else {
-    audio.pause();
-    playing = false;
+    finish();
   }
-});
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Data do início do relacionamento (substitua com a data real)
-  var startDate = new Date("2020-03-27"); // Substitua YYYY-MM-DD pela data de início
-
-  // Função para calcular a diferença entre as datas em dias
-  function calcularDiferencaEmDias(data1, data2) {
-      var diffEmMilliseconds = Math.abs(data2 - data1);
-      return Math.ceil(diffEmMilliseconds / (1000 * 60 * 60 * 24));
+function finish() {
+  if (questionsCorrect >= 5){
+    textFinish.innerHTML = `Você acertou ${questionsCorrect} de ${questions.length}. Parabéns, você conhece essa beldade.` ;
+    content.style.display = "none";
+    contentFinish.style.display = "flex";
+    botao__reiniciar.style.display = "flex";
+    
+  } else {
+    textFinish.innerHTML = `Você acertou ${questionsCorrect} de ${questions.length}. Vamos conversar mais com a Damola, por favor.`;
+    content.style.display = "none";
+    contentFinish.style.display = "flex";
+    botao__reiniciar.style.display = "flex";
   }
+}
 
-  // Função para atualizar o contador de dias
-  function atualizarContador() {
-      var currentDate = new Date();
-      var daysTogether = calcularDiferencaEmDias(startDate, currentDate);
-      document.getElementById("contador").textContent = "Já estamos de lovezinho há " + daysTogether + " dias!";
-  }
+function loadQuestion() {
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
 
-  // Atualizar o contador de dias inicialmente
-  atualizarContador();
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
 
-  // Atualizar o contador de dias a cada segundo
-  setInterval(atualizarContador, 1000);
-});
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
+}
+
+loadQuestion();
+
+
+
+
+//   // Função para atualizar o contador de dias
+//   function atualizarContador() {
+//       var currentDate = new Date();
+//       var daysTogether = calcularDiferencaEmDias(startDate, currentDate);
+//       document.getElementById("contador").textContent = "Já estamos de lovezinho há " + daysTogether + " dias!";
+//   }
+
+//   // Atualizar o contador de dias inicialmente
+//   atualizarContador();
+
+//   // Atualizar o contador de dias a cada segundo
+//   setInterval(atualizarContador, 1000);
+// });
---
+++ js/questoes.js
@@ -0,0 +1,82 @@
+export default [
+    {
+      question: "Qual a banda preferida de Daminha?",
+      answers: [
+        { option: "Slaughter", correct: true },
+        { option: "Iron Maiden", correct: false },
+        { option: "Skid Row", correct: false },
+      ],
+    },
+    {
+      question: "Qual a comida favorita de Daminha?",
+      answers: [
+        { option: "Hamburguer", correct: false },
+        { option: "Pizza", correct: true },
+        { option: "Lasanha", correct: false },
+      ],
+    },
+    {
+      question: "Qual o nome da cachorrinha da Daminha?",
+      answers: [
+        { option: "Peposa", correct: false },
+        { option: "Meg", correct: false },
+        { option: "Luna", correct: true },
+      ],
+    },
+    {
+      question: "Eu sou o ...",
+      answers: [
+        { option: "Maioral", correct: false },
+        { option: "Gostosão", correct: false },
+        { option: "Caçador", correct: true },
+      ],
+    },
+    {
+      question: "Qual a cor favorita de Daminha?",
+      answers: [
+        { option: "Amarelo", correct: false },
+        { option: "Vermelho", correct: false },
+        { option: "Rosa", correct: true },
+      ],
+    },
+    {
+      question: "Qual desses ela escolheria?",
+      answers: [
+        { option: "Popeyes", correct: true },
+        { option: "McDonalds", correct: false },
+        { option: "Burguer King", correct: false },
+      ],
+    },
+    {
+      question: "Com qual boneco ela tem menos horas de ranked?",
+      answers: [
+        { option: "Kayo", correct: false },
+        { option: "Fade", correct: false },
+        { option: "Omen", correct: true },
+      ],
+    },
+    {
+      question: "Quantas tatuagens tem Daminha?",
+      answers: [
+        { option: "8", correct: true },
+        { option: "3", correct: false },
+        { option: "6", correct: false },
+      ],
+    },
+    {
+      question: "Qual o signo de Daminha?",
+      answers: [
+        { option: "Touro", correct: true },
+        { option: "Peixes", correct: false },
+        { option: "Sagitário", correct: false },
+      ],
+    },
+    {
+      question: "Se ela fosse um pão, qual pão ela seria?",
+      answers: [
+        { option: "Mole", correct: false },
+        { option: "Molhado", correct: true },
+        { option: "Duro", correct: false },
+      ],
+    },
+  ];
